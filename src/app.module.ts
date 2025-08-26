b43f21489d73248410a4a5a99ab06c5b36f88bc4
pungkk <psso2004@superbullet.co.kr>
2025-08-26 11:11:05 +0900

    feat: import typeorm module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,9 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
+import { DataSource, DataSourceOptions } from 'typeorm';
 import { AppController } from './app.controller';
 import { AppResolver } from './app.resolver';
 import { AppService } from './app.service';
@@ -24,8 +26,28 @@ import { BoardModule } from './board/board.module';
             ? true
             : join(process.cwd(), 'schema.gql'),
         playground: false,
+        // todo: 운영 환경에서는 비활성화 해야됨.
         plugins: [ApolloServerPluginLandingPageLocalDefault()],
       }),
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        logging: ['error', 'query'],
+        synchronize: false,
+      }),
+      dataSourceFactory: async (options: DataSourceOptions) => {
+        const dataSource = new DataSource(options);
+        await dataSource.initialize();
+        return dataSource;
+      },
     }),
     BoardModule,
   ],
